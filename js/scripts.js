09ced462e0ee0d00a6d3d648951deacba82e4b7a
Rickzerr94 <rickzerr91@gmail.com>
2022-11-01 22:57:54 +0200

    added dialog on button

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,21 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
+    });
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
